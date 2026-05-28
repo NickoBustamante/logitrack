872e130ad13b477faee00fedf4184bfcdb322c76
Pedro Nicolás Bustamante Valverdi <nickobustamante@gmail.com>
2026-05-28 20:18:45 +0200

    Añade ejemplo de configuración e ignora conexión local

---
+++ config/conexion.example.php
@@ -0,0 +1,15 @@
+<?php
+$host = "127.0.0.1";
+$usuario = "TU_USUARIO";
+$contrasena = "TU_CONTRASEÑA";
+$basedatos = "logitrack";
+$puerto = 3307;
+
+$conexion = new mysqli($host, $usuario, $contrasena, $basedatos, $puerto);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+$conexion->set_charset("utf8mb4");
+?>
